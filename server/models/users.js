83466be44c833e8509ebaec6e5480acf810f5737
cam <cam@camherbert.com>
2022-01-15 11:08:36 -0500

    changed username to email

--- server/models/users.js
+++ server/models/users.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
     {
-        userName: {
+        email: {
             type: String,
             required: true,
             trim: true,
