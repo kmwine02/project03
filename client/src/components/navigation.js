29fb44aad21c613b25610b462b1d75a5470acc35
Sean M <mctastic2121@gmail.com>
2022-01-15 11:27:21 -0500

    added {} around NavLink import

--- client/src/components/navigation.js
+++ client/src/components/navigation.js
@@ -1,6 +1,6 @@
 import React from "react";
 import menu from "../data/menu";
-import NavLink from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 export default function Navigation() {
   return (
