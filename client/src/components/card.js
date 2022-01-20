fc0c9ecb6a584aa5e75766e8e6a756609ea42f5f
Steven Sosebee <swsosebee@gmail.com>
2022-01-20 12:01:58 -0500

    making user rating available for mutations

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,24 +1,22 @@
 import React from "react";
 import MovieRating from "./rating";
 
-
 export default function Card({ movieData }) {
   return (
     <ul>
       {movieData.map((movie) => (
         <li className="movie-card" key={movie.title}>
-          <img src= {movie.image} alt="box-art"></img>
+          <img src={movie.image} alt="box-art"></img>
           <div className="card-text">
             <p>
               <span>{movie.title}</span>
               <br></br>
               <span>{movie.rating}</span>
             </p>
-            <MovieRating />
+            <MovieRating movie={movie.title} />
           </div>
         </li>
       ))}
     </ul>
-
   );
 }
--- client/src/components/rating.js
+++ client/src/components/rating.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Rating from "react-rating-scale";
 
-export default function MovieRating() {
+export default function MovieRating({ movie }) {
+  const [userRating, setUserRating] = useState(0);
+
+  const callRating = (e) => {
+    const rating = e;
+    console.log(`User rates ${movie} - ${rating} trees!`);
+  };
   return (
     <div>
-      <Rating length={5} width={30} />
+      <Rating
+        length={5}
+        // width={30}
+        value={userRating}
+        onSelect={callRating}
+      />
     </div>
   );
 }
