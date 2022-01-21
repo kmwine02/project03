6e36c58dbe60e6f61c09c24269a19bfdf0c27d6a
Sean M <mctastic2121@gmail.com>
2022-01-20 23:00:37 -0500

    changed error to a console.log

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
   }
   catch (error) {
-    res.status(500).send(error)
+    console.log(error)
   }
 });
 };
