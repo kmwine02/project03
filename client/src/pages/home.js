5b10ab94d3a29b9cbbc2271d96325fc0d3d39b84
Sean M <mctastic2121@gmail.com>
2022-01-24 16:58:20 -0500

    added homepage styling and small card styling

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,5 +1,21 @@
 import React from "react";
+import SnowFlakes from "../components/Snow";
+import { NavLink } from "react-router-dom";
+import SnowGlobe from "../images/200.gif"
+import Button from "react-bootstrap/Button";
+import "../components/css/home.css";
 
 export default function HomePage() {
-  return <div>Christmas Movie Rating Database!</div>;
+  return (
+    <>
+      <SnowFlakes />
+      <div className="homeContainer">
+          <h1>Welcome to the Christmas Movie Workshop!</h1>
+          <div className="snowGlobe">
+            <img src={SnowGlobe}/>
+          </div>
+          <NavLink to="/search"><Button className="homeButton" variant="success" size="lg">Get Started!</Button>{' '}</NavLink>
+      </div>
+    </>
+  );
 }
